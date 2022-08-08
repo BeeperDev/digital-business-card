7bced9b53b212dbad0d086a0e7e1f501e7ff7bc6
Erica Layton <e.alexislayton@gmail.com>
2022-08-08 14:09:06 -0700

    add About and Interests components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Info from "./components/Info";
+import About from "./components/About";
+import Interests from "./components/Interests";
 
 function App() {
   return (
     <div className="app-container">
       <Info />
+      <About />
+      <Interests />
     </div>
   );
 }
